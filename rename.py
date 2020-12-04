94c76310a63e46bd81620766b8f58497e1981c65
stewilliams-extr <stewilliams@extremenetworks.com>
2020-12-04 08:28:03 -0500

    Create rename.py

---
+++ rename.py
@@ -0,0 +1,53 @@
+import os
+import re
+import sys
+import platform
+"""I made this script because I was tired of removing some small text
+from A long list of filenames.
+
+To use this program correctly, go to the folder where you want to remove text from
+files and execute the python file.  Before the program makes changes it will tell
+you what it's going to change before it does it.
+"""
+#Sorry no support for windows right now.
+if platform.system() == 'Windows':
+    print ("This scrpt is currently only supported on Linux")
+    sys.exit()
+
+#this raw_input captures the string to remove
+text_remove = raw_input('What string would you like to remove (Use "quotes if there is a space".)? ')
+
+#initializing the lists
+change = []
+nochange = []#I don't use this but it could be useful
+commands = []
+
+#This for loop captures all the file names in the dir and fills up the list above.
+for filename in os.listdir("."):
+
+    if text_remove in filename:
+        new_name = re.sub(text_remove, '', filename)
+        change.append(new_name)
+        commands.append(("mv \"{0}\" \"{1}\"").format(filename,new_name))
+    else:
+        nochange.append(filename)
+#Checking for a match and printing a warning to let you know what the script woud do.
+if len(commands) > 0:
+    print ("")
+    print ("")
+    print ("Commands to be executed:")
+    for command in commands:
+        print command
+    print ("")
+    cont = raw_input('Would you like to continue (y/n)? ')
+else:
+    print ("No matches found")
+    quit()
+#if y then we run the commands.
+if cont.lower() == 'y':
+    for command in commands:
+        os.system(command)
+
+#Error capture
+else:
+    print ("You selected n or an invalid value. Script Stopped")
